2c8f5a557a4309b3fa5243d46bf6cbb2ccb92588
Henrique Custodio <henrique.spilere.custodio@gmail.com>
2022-11-18 19:05:22 -0300

    Fist component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,14 @@
+import NotificationButton from "./componentes/NotificationButton"
+
 function App() {
 
 
   return (
-    <h1>Ola mundo</h1>
+    <>
+      <h1>Ola mundo</h1>
+       <NotificationButton/>
+       <NotificationButton/>
+  </>
   )
 }
 
